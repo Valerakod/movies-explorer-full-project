9ff550857551a09631c96608a18498a787ffa2ce
Valeriya Yemets <valerakod30@gmail.com>
2023-08-25 15:51:14 +0100

    Full project

--- backend/middlewares/error.js
+++
@@ -1,9 +0,0 @@
-const MESSAGES = require('../utils/messages');
-
-module.exports = (err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode).send({
-    message: statusCode === 500 ? MESSAGES.SERVER_ERROR : message,
-  });
-  next(err);
-};
